e1142ce229e02845e0a2d08a7ad0e7f7e0048189
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-12-01 17:11:44 +0100

    creata un istanza della classe ACQUA e svolta la prova dei suoi metodi e del metodo in override StampaProdotto

--- CSharpShop 2/Program.cs
+++ CSharpShop 2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using CSharpShop_2;
+
+// CREIAMO L'OGGETTO BOTTIGLIA1
+Acqua bottiglia1 = new Acqua("Acqua Gaia", "acqua frizzante", 1, 22, "Genga (AN)", 1, 1.5);
+
+bottiglia1.StampaPrezzoBase();
+bottiglia1.StampaPrezzoIva();
+bottiglia1.StampaProdotto();
+
+// BEVIAMO DELL'ACQUA
+bottiglia1.Bevi(0.5);
+bottiglia1.StampaProdotto();
+
+// SVUOTIAMO LA BOTTIGLIA D'ACQUA
+bottiglia1.Svuota();
+bottiglia1.StampaProdotto();
+
+// RIEMPIAMO LA BOTTIGLIA D'ACQUA
+bottiglia1.Riempi(2);
+bottiglia1.StampaProdotto();
